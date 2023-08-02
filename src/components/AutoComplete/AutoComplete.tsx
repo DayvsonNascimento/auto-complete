2c6e2001681760987c703ea50b5b272b2caec64c
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2023-08-02 19:04:15 -0300

    feat: add listing and basic filtering from api results

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,26 +1,70 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 import styles from './AutoComplete.module.css';
 
+interface Movie {
+  title: string;
+}
+
 const AutoComplete = () => {
   const [active, setActive] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [showOtpions, setShowOptions] = useState(false);
+  const [query, setQuery] = useState('');
 
   const handleFocus = () => {
     setActive(true);
+    setShowOptions(true);
   };
 
-  const handleOnBlur = () => setActive(false);
+  const handleOnBlur = () => {
+    setActive(false);
+    setShowOptions(false);
+  };
+
+  const getOptionsbyQuery = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3004/movies?title_like=${query}`
+      );
+      const data = await response.json();
+
+      const moviesTitles = data?.map((movie: Movie) => movie.title) || [];
+
+      setOptions(moviesTitles);
+    } catch (error) {
+      console.error(error);
+
+      setOptions([]);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    getOptionsbyQuery();
+  }, [query]);
 
   return (
     <div className={styles.container}>
-      <label className={`${styles.label} ${active && styles.active} `}>
+      <label
+        className={`${styles.label} ${(active || query) && styles.active} `}
+      >
         Options
       </label>
       <input
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
         className={styles.input}
         onFocus={handleFocus}
         onBlur={handleOnBlur}
       ></input>
+
+      {showOtpions && (
+        <div className={styles.resultsContainer}>
+          {options.map((option) => (
+            <div className={styles.resultItem}>{option}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
