a2caeac1654461a07430ef284e096d0a21a90a6f
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2023-08-02 13:15:12 -0300

    feat: create AutoComplete component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import AutoComplete from './components/AutoComplete/AutoComplete';
+
+const App = () => {
   return (
     <div>
       <h1>Auto Complete</h1>
+      <AutoComplete />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import styles from './AutoComplete.module.css';
+
+const AutoComplete = () => {
+  const [active, setActive] = useState(false);
+
+  const handleFocus = () => {
+    setActive(true);
+  };
+
+  const handleOnBlur = () => setActive(false);
+
+  return (
+    <div className={styles.container}>
+      <label className={`${styles.label} ${active && styles.active} `}>
+        AutoComplete
+      </label>
+      <input
+        className={styles.input}
+        onFocus={handleFocus}
+        onBlur={handleOnBlur}
+      ></input>
+    </div>
+  );
+};
+
+export default AutoComplete;
